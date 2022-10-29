552071d4bdbdd2de23c0ad52e5a1b8c983969097
TeaChareqishvili <teachareqishvili89@gmail.com>
2022-10-30 03:34:20 +0400

    if/else

---
+++ main.js
@@ -0,0 +1,51 @@
+//1//
+
+const currentDay = new Date();
+const day = currentDay.getDay();
+
+switch (currentDay) {
+    case 0:
+        day = 'Sunday';
+        break;
+        case 1:
+            day = 'Monday';
+            break;
+            case 2:
+                day = 'Tuesday';
+                break;
+                case 3:
+                    day = 'Wednesday';
+                    break;
+                    case 4:
+                        day = 'Thursday';
+                        break;
+                        case 5:
+                        day = 'friday';
+                        break;
+                        case 6:
+                            day = 'Saturday';
+
+}
+// console.log (day);
+
+// 2 // 
+
+for ( let i = 0; i < 50; i++){
+    //   console.log(i);
+
+}
+
+// 3 // 
+
+let i = 0;
+
+while (i <150  ){
+    i+=1;
+   
+    
+   
+    // console.log(i);
+ }
+
+
+   
